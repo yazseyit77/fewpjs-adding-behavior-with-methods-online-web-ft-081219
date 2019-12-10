cb75678366cc0e1ef1c54281028933f0b8a914a0
yazseyit77 <yazgeldi.seyidov@gmail.com>
2019-12-09 23:30:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+
+let garfield = new Cat('Garfield', 'Female');
+
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+}
+
+let alabay = new Dog('Alabay', 'Male');
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        if (this.sex === 'male') {
+            return `It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+    }
+}
+
+let cookarikoo = new Bird('Jack', 'Male')
